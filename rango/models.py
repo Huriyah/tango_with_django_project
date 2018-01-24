27417f016e9c28fbf1f9c89dc1ee193c08f1a0d8
Huriyah Hussain <2241433h@student.gla.ac.uk>
2018-01-24 14:02:11 +0000

    Chapter 5

--- rango/models.py
+++ rango/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
     
     def __str__(self):
         return self.name
@@ -18,4 +20,5 @@ class Page(models.Model):
 
     
     def __str__(self):
-        return self.title
+        return self.title
+
